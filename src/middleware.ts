67fb36402665e5339979503853328215d056dced
Priyank-a-a <mishra.priya32@rediffmail.com>
2025-09-29 23:34:13 +0530

    resolved signup issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const publicPaths = ["/login"];
+const publicPaths = ["/login", "/signup"];
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
